ece05abde834daef507281328541a1671d43d768
Whien <sal95610@gmail.com>
2019-10-20 10:43:49 +0800

    First: first commit

---
+++ precache-manifest.fec33cc1debf9b6222fb7afd8a648fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058fc52e05143a78f1a73b749cdfc123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "febe186ef9b24f28e84d",
+    "url": "/static/css/2.f1a76ffa.chunk.css"
+  },
+  {
+    "revision": "94e79af9feffa9712f1e",
+    "url": "/static/css/main.42b35307.chunk.css"
+  },
+  {
+    "revision": "febe186ef9b24f28e84d",
+    "url": "/static/js/2.e4b9bc86.chunk.js"
+  },
+  {
+    "revision": "94e79af9feffa9712f1e",
+    "url": "/static/js/main.7040c97e.chunk.js"
+  },
+  {
+    "revision": "7f6cc280bdd5297e79a9",
+    "url": "/static/js/runtime-main.bd15e5d7.js"
+  }
+]);
